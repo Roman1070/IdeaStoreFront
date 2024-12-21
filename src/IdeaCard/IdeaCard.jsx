7f936ed704b23a3da491628c1de0d66418049939
Roman <r.yakushin.1070@mail.ru>
2024-12-21 16:36:49 +0300

    Create IdeaCard.jsx

---
+++ src/IdeaCard/IdeaCard.jsx
@@ -0,0 +1,61 @@
+import { useEffect } from "react";
+import "./IdeaCard.css";
+
+export default function IdeaCard({
+  image,
+  onMouseEnter,
+  onMouseExit,
+  index,
+  isSelected,
+}) {
+  return (
+    <div
+      onMouseEnter={() => onMouseEnter(index)}
+      onMouseLeave={() => onMouseExit(index)}
+      className={"ideaCard" + (isSelected ? " activeIdeaCard" : "")}
+      style={{
+        display: "flex",
+        height: "max-content",
+        justifyContent: "center",
+        backgroundImage: { image },
+        position: "relative",
+      }}
+    >
+      {isSelected && (
+        <>
+          <img
+            src={image}
+            alt={image}
+            style={{
+              color: "#000",
+              width: "100%",
+              borderRadius: "20px",
+              opacity: 1,
+              zIndex: "1",
+            }}
+          />
+
+          <div
+            style={{
+              position: "absolute",
+              width: "130px",
+              height: "55px",
+              top: "10px",
+              right: "10px",
+              zIndex: "5",
+            }}
+          >
+            <button className="saveButton">Сохранить</button>
+          </div>
+        </>
+      )}
+      {!isSelected && (
+        <img
+          src={image}
+          alt={image}
+          style={{ width: "100%", borderRadius: "20px" }}
+        />
+      )}
+    </div>
+  );
+}

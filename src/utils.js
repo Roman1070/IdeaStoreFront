43bda18c6631b9742a71898ad549dba15fdd3f75
Roman <r.yakushin.1070@mail.ru>
2025-02-05 21:52:18 +0300

    requests on local host

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,8 @@
 export function GetImageSrc(name) {
-  return "http://ideastore.space/app/files/" + name;
+  return "http://0.0.0.0/app/files/" + name;
 }
 
-const ImagesFolderName = "http://ideastore.space/images/";
+const ImagesFolderName = "http://0.0.0.0/images/";
 export function GetLocalImageSrc(name) {
   return ImagesFolderName + name;
 }
@@ -31,15 +31,15 @@ export function DeleteCookie(name, path, domain) {
   } else console.log("cant find cookie " + name);
 }
 export function JoinReactHostAddress(endpoint) {
-  const ClientAddress = "http://ideastore.space/";
+  const ClientAddress = "http://0.0.0.0/";
   return ClientAddress + endpoint;
 }
 export function JoinClientAddress(endpoint) {
-  const ClientAddress = "http://ideastore.space/api/v1/";
+  const ClientAddress = "http://0.0.0.0/api/";
   return ClientAddress + endpoint;
 }
 export function GetChatWebSocketAddress() {
-  return "ws://ideastore.space/api/v1/chat_ws";
+  return "ws://0.0.0.0/api/chat_ws";
 }
 
 export function Morph(int, array) {

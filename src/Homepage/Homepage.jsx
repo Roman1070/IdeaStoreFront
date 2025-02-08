7815b25f77de9f70c42ccd04fc953dbeeed06323
Roman <r.yakushin.1070@mail.ru>
2025-02-09 02:26:43 +0300

    increase throttle duration

--- src/Homepage/Homepage.jsx
+++ src/Homepage/Homepage.jsx
@@ -15,7 +15,7 @@ export function FetchData(ideasToLoad, onComplete) {
 export default function Homepage({ foundIdeas, searchInput }) {
   const [ideas, setIdeas] = useState([]);
   const [boards, setBoards] = useState([]);
-  const throttleDelay = 2000;
+  const throttleDelay = 5000;
 
   var loadedIdeasCount = 50;
   if (ideas.length == 0 && boards.length == 0) {
--- src/LandingPage/LandingPage.jsx
+++ src/LandingPage/LandingPage.jsx
@@ -8,7 +8,7 @@ import { FetchData } from "../Homepage/Homepage";
 
 export default function LandingPage() {
   const [ideas, setIdeas] = useState();
-  const throttleDelay = 2000;
+  const throttleDelay = 5000;
   var loadedIdeasCount = 50;
   console.log(ideas);
 
